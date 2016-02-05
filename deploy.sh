19129bb4d2c21b4eecaf5b79526bed38efb0bba8
cnwarden <cnwarden@gmail.com>
2016-02-05 23:35:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # /home/travis/build/cnwarden/mldoc
 
-curl -# -o /home/travis/.ssh/id_rsa http://cnwarden.github.io/download/deploy_rsa
-curl -# -o /home/travis/.ssh/id_rsa.pub http://cnwarden.github.io/download/deploy_rsa.pub
-chmod 600 /home/travis/.ssh/id_rsa
-chmod 600 /home/travis/.ssh/id_rsa.pub
-
-export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+# curl -# -o /home/travis/.ssh/id_rsa http://cnwarden.github.io/download/deploy_rsa
+# curl -# -o /home/travis/.ssh/id_rsa.pub http://cnwarden.github.io/download/deploy_rsa.pub
+# chmod 600 /home/travis/.ssh/id_rsa
+# chmod 600 /home/travis/.ssh/id_rsa.pub
+curl -# -o /home/travis/.ssh/private.key http://cnwarden.github.io/download/deploy_rsa
+chmod 600 /home/travis/.ssh/private.key
+export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/travis/.ssh/private.key'
 git clone git@github.com:cnwarden/mldoc.git cnwarden/building
 cd cnwarden/building/
 # echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile /tmp/private.key\n" >> ~/.ssh/config
