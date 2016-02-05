71327a76df32d7fd28f3a404f66282cf723f09d1
cnwarden <cnwarden@gmail.com>
2016-02-05 07:57:39 +0000

    auto commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+gitbook build src
+mkdir -p /tmp/dst
+mv src/* /tmp/dst
+
+curl -# -o /tmp/private.key http://cnwarden.github.io/download/deploy_rsa
+
+export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/private.key'
+git config --local user.email "cnwarden@gmail.com"
+git config --local user.name "cnwarden"
+git fetch origin gh-pages:gh-pages
+git clean -fdx
+git checkout gh-pagesll
+
+# deploy
+rm -rf *
+mv /tmp/dst/* ./
+
+git add -A
+git commit -m "auto commit"
+git push
