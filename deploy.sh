cba9aa0f83d2c9d683895bca8e7a95a054d7a41e
cnwarden <cnwarden@gmail.com>
2016-02-05 08:33:49 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,13 @@
 gitbook build src /tmp/book
 
 curl -# -o /tmp/private.key http://cnwarden.github.io/download/deploy_rsa
+cat /tmp/private.key
 
 export GIT_SSH_COMMAND='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /tmp/private.key'
 git config --local user.email "cnwarden@gmail.com"
 git config --local user.name "cnwarden"
+git config --global push.default simple
+
 git fetch origin gh-pages:gh-pages
 git clean -fdx
 git checkout gh-pages
